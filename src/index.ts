aae63bb3dbea024008d05d43cdb7ec56d82e152c
kizuna <syahrur.ramadhan94@gmail.com>
2025-12-15 16:15:05 +0800

    pretty much done, just need to test it

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import moviesRouter from "./routes/movies.route";
 import movieRouter from "./routes/movie.route";
 import { notFound } from "./middlewares/notFound";
 import { errorHandler } from "./middlewares/error";
+import * as db from "./db/db"
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -12,8 +13,9 @@ const PORT = Number(process.env.PORT) || 3000;
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (_req, res) => {
-  res.json({ status: "ok" });
+app.get("/", async (_req, res) => {
+  const { rows } = await db.query("SELECT NOW()")
+  res.json(rows[0]);
 });
 
 app.use("/movies", moviesRouter);
